c47f134f05563128f94520bb4c13bd52d947d0ef
stevenfranks <stevenfranks@users.noreply.github.com>
2019-10-12 19:23:11 +0100

    Move service connection ID in main

--- .azdo/main.tf
+++ .azdo/main.tf
@@ -23,12 +23,12 @@ resource "azuredevops_build_definition" "nightly_build" {
   project_id      = azuredevops_project.project.id
   agent_pool_name = "Hosted Ubuntu 1604"
   name            = "Nightly Build"
+  service_connection_id = azuredevops_serviceendpoint.github_serviceendpoint.id  
 
   repository {
     repo_type             = "GitHub"
     repo_name             = "microsoft/terraform-provider-azuredevops"
     branch_name           = "master"
     yml_path              = ".azdo/azure-pipeline-nightly.yml"
-    service_connection_id = azuredevops_serviceendpoint.github_serviceendpoint.id
   }
 }
